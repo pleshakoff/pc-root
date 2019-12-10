153b31aef29c55cf1d91c54eaf182e70ee307204
pleshakoff <pleshakoff@gmail.com>
2019-12-10 23:48:33 +0300

    compose без security

--- build_and_push.cmd
+++ build_and_push.cmd
@@ -25,7 +25,7 @@ docker image build -t pleshakoff/pc-notifier-agent-email:%branch% .
 docker image push pleshakoff/pc-notifier-agent-email:%branch%
 
 echo pc-notifier-agent-push
-cd  d:\jprojects\parcom\pc-notifier-agent-email\
+cd  d:\jprojects\parcom\pc-notifier-agent-push\
 git -c "credential.helper=C:/Program\ Files/SmartGit/lib/credentials.cmd" checkout %branch%
 docker image build -t pleshakoff/pc-notifier-agent-push:%branch% .
 docker image push pleshakoff/pc-notifier-agent-push:%branch%
---
+++ db_kill.cmd
@@ -0,0 +1,5 @@
+docker stop local_classroom_pg
+docker rm local_classroom_pg
+docker stop local_security_pg
+docker rm local_security_pg
+docker container prune 
