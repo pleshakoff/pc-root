5eec9172c2ead3ede2220ee04725e71bfdff2786
pleshakoff <pleshakoff@gmail.com>
2019-12-10 19:58:38 +0300

    build_and_push notifier agents

--- build_and_push.cmd
+++ build_and_push.cmd
@@ -6,7 +6,6 @@ git -c "credential.helper=C:/Program\ Files/SmartGit/lib/credentials.cmd" checko
 docker image build -t pleshakoff/pc-classroom:%branch% .
 docker image push pleshakoff/pc-classroom:%branch%
 
-
 echo security
 cd  d:\jprojects\parcom\pc-security\
 git -c "credential.helper=C:/Program\ Files/SmartGit/lib/credentials.cmd" checkout %branch%
@@ -18,3 +17,18 @@ cd  d:\jprojects\parcom\pc-notifier\
 git -c "credential.helper=C:/Program\ Files/SmartGit/lib/credentials.cmd" checkout %branch%
 docker image build -t pleshakoff/pc-notifier:%branch% .
 docker image push pleshakoff/pc-notifier:%branch%
+
+echo pc-notifier-agent-email
+cd  d:\jprojects\parcom\pc-notifier-agent-email\
+git -c "credential.helper=C:/Program\ Files/SmartGit/lib/credentials.cmd" checkout %branch%
+docker image build -t pleshakoff/pc-notifier-agent-email:%branch% .
+docker image push pleshakoff/pc-notifier-agent-email:%branch%
+
+echo pc-notifier-agent-push
+cd  d:\jprojects\parcom\pc-notifier-agent-email\
+git -c "credential.helper=C:/Program\ Files/SmartGit/lib/credentials.cmd" checkout %branch%
+docker image build -t pleshakoff/pc-notifier-agent-push:%branch% .
+docker image push pleshakoff/pc-notifier-agent-push:%branch%
+
+
+cd d:\jprojects\parcom\pc-root\
